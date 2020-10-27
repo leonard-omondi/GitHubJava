1654c7ea204e4d5649c7e00040a9dafe588d93a5
Leonard Omondi <lomondi@students.kennesaw.edu>
2020-10-27 10:13:09 -0400

    updated by user LO.

--- src/com/Omondi/Main.java
+++ src/com/Omondi/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+        System.out.println(sum);
     }
 }
